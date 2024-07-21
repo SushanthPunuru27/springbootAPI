9794f0950f9c346b819a776d18cd6afcebd37d9a
SushanthPunuru98 <punurusushanth@gmail.com>
2024-07-21 17:14:16 -0400

    Validation added

--- src/main/java/com/springboot/restapi/entity/Student.java
+++ src/main/java/com/springboot/restapi/entity/Student.java
@@ -6,7 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
 @Entity
 @Table(name = "student")
 public class Student {
@@ -20,55 +25,57 @@ public class Student {
 	private float percentage;
 	@Column(name = "branch")
 	private String branch;
+//We have used lombok dependency in our pom.xml so no need to use getters and setters.
+	
+	
+//	public Student() {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public Student(int rollNo, String name, float percentage, String branch) {
+//		super();
+//		this.rollNo = rollNo;
+//		this.name = name;
+//		this.percentage = percentage;
+//		this.branch = branch;
+//	}
 
-	public Student() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Student(int rollNo, String name, float percentage, String branch) {
-		super();
-		this.rollNo = rollNo;
-		this.name = name;
-		this.percentage = percentage;
-		this.branch = branch;
-	}
-
-	public int getRollNo() {
-		return rollNo;
-	}
-
-	public void setRollNo(int rollNo) {
-		this.rollNo = rollNo;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public float getPercentage() {
-		return percentage;
-	}
-
-	public void setPercentage(float percentage) {
-		this.percentage = percentage;
-	}
-
-	public String getBranch() {
-		return branch;
-	}
-
-	public void setBranch(String branch) {
-		this.branch = branch;
-	}
-
-	@Override
-	public String toString() {
-		return "Student [rollNo=" + rollNo + ", name=" + name + ", percentage=" + percentage + ", branch=" + branch
-				+ "]";
-	}
+//	public int getRollNo() {
+//		return rollNo;
+//	}
+//
+//	public void setRollNo(int rollNo) {
+//		this.rollNo = rollNo;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public float getPercentage() {
+//		return percentage;
+//	}
+//
+//	public void setPercentage(float percentage) {
+//		this.percentage = percentage;
+//	}
+//
+//	public String getBranch() {
+//		return branch;
+//	}
+//
+//	public void setBranch(String branch) {
+//		this.branch = branch;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Student [rollNo=" + rollNo + ", name=" + name + ", percentage=" + percentage + ", branch=" + branch
+//				+ "]";
+//	}
 
 }
